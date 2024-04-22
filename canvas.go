494ba166a740d82a19fbbc722c33b033605cf5bd
Iskander Sharipov <quasilyte@gmail.com>
2024-04-22 15:05:03 +0400

    add experimental Canvas type

---
+++ canvas.go
@@ -0,0 +1,66 @@
+package graphics
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/quasilyte/gmath"
+)
+
+type Canvas struct {
+	Pos gmath.Pos
+
+	Rotation *gmath.Rad
+
+	spr       *Sprite
+	container *Container
+}
+
+func NewCanvas(cache *Cache) *Canvas {
+	return &Canvas{
+		spr:       NewSprite(cache),
+		container: NewContainer(),
+	}
+}
+
+func (c *Canvas) SetDstImage(img *ebiten.Image) {
+	c.spr.SetImage(img)
+}
+
+func (c *Canvas) IsDisposed() bool {
+	return c.container.IsDisposed()
+}
+
+func (c *Canvas) Dispose() {
+	c.container.Dispose()
+}
+
+func (c *Canvas) IsVisible() bool {
+	return c.container.IsVisible()
+}
+
+func (c *Canvas) SetVisibility(visible bool) {
+	c.container.SetVisibility(visible)
+}
+
+func (c *Canvas) Draw(dst *ebiten.Image) {
+	c.DrawWithOptions(dst, DrawOptions{})
+}
+
+func (c *Canvas) AddChild(o object) {
+	c.container.AddChild(o)
+}
+
+func (c *Canvas) DrawWithOptions(dst *ebiten.Image, opts DrawOptions) {
+	if !c.container.IsVisible() {
+		return
+	}
+
+	c.spr.GetImage().Clear()
+
+	opts.Offset = opts.Offset.Add(c.Pos.Resolve())
+	if c.Rotation != nil {
+		opts.Rotation += *c.Rotation
+	}
+	c.container.DrawWithOptions(c.spr.GetImage(), opts)
+
+	c.spr.Draw(dst)
+}
